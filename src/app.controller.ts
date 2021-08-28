b6152ef9ecc218f9c14ebf5f92d76f306bce4144
Nick Freitas <nickfrei@gmail.com>
2021-08-28 11:53:34 -0400

    cleanup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,18 +2,13 @@ import {
   GamebookCreatedEvent,
   GamebookUpdatedEvent,
   isGamebookCreatedEvent,
-  isGamebookUpdatedEvent,
   isMakeChoiceEvent,
   isResetChoicesEvent,
   isUndoChoiceEvent,
-  isUserCreatedEvent,
-  isUserUpdatedEvent,
   MakeChoiceEvent,
   ResetChoicesEvent,
   Topics,
   UndoChoiceEvent,
-  UserCreatedEvent,
-  UserUpdatedEvent,
 } from '@indigobit/nubia.common';
 import { BadRequestException, Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
@@ -28,19 +23,22 @@ export class AppController {
     @Payload()
     { value }: { value: any },
   ): any {
-    const { type, data } = value;
+    const { type, data, auth } = value;
     if (!type) {
       throw new BadRequestException('Missing "type" in UserEvent');
     }
 
     if (isResetChoicesEvent(value)) {
-      return this.appService.resetChoices(data as ResetChoicesEvent['data']);
+      return this.appService.resetChoices(
+        data as ResetChoicesEvent['data'],
+        auth,
+      );
     }
     if (isUndoChoiceEvent(value)) {
-      return this.appService.undoChoice(data as UndoChoiceEvent['data']);
+      return this.appService.undoChoice(data as UndoChoiceEvent['data'], auth);
     }
     if (isMakeChoiceEvent(value)) {
-      return this.appService.makeChoice(data as MakeChoiceEvent['data']);
+      return this.appService.makeChoice(data as MakeChoiceEvent['data'], auth);
     }
 
     console.log(type);
@@ -63,7 +61,7 @@ export class AppController {
     if (isGamebookCreatedEvent(value)) {
       return this.appService.gamebookCreatedHandler(
         data as GamebookCreatedEvent['data'],
-        auth as GamebookCreatedEvent['auth'],
+        auth,
       );
     }
 
